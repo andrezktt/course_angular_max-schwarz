16f031af855f58f14f2ea338822b65a397a63145
andre_zktt <andrezicatti@gmail.com>
2025-04-16 17:04:19 -0300

    :recycle: refactor: Storing Data Models in Separate Files

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,16 +1,5 @@
 import {Component, computed, EventEmitter, input, Input, output, Output} from '@angular/core';
-
-// type User = {
-//   id: string;
-//   avatar: string;
-//   name: string;
-// }
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { type User } from "./user.model";
 
 @Component({
   selector: 'app-user',
@@ -21,17 +10,6 @@ interface User {
 })
 export class UserComponent {
 
-  // @Input({ required: true }) id!: string;
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) name!: string;
-
-  // SIGNALS
-  // avatar = input.required<string>();
-  // name = input.required<string>();
-  // id = input.required<string>();
-
-  // select = output<string>()
-
   @Input({ required: true }) user!: User;
 
   @Output() select = new EventEmitter<string>();
@@ -39,11 +17,6 @@ export class UserComponent {
   get imagePath() {
     return 'assets/users/' + this.user.avatar;
   }
-
-  // Using signals
-  // imagePath = computed(() => {
-  //   return 'assets/users/' + this.avatar();
-  // });
 
   onSelectUser() {
     this.select.emit(this.user.id);
